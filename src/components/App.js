54d662adc51f63b3c40997d39821905b54372b89
Sandeep <newsandeep03@gmail.com>
2023-07-11 21:51:42 +0530

    ans add

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,12 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [time, setTime] = useState(0);
+
   return (
     <div>
+      <p>Button clicked {time} times</p>
+      <button onClick={() => {setTime(time+1)}}>Click me</button>
         {/* Do not remove the main div */}
     </div>
   )
