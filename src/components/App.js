1434afa6b6013db77346fc22424417b1001f5218
Sandeep <newsandeep03@gmail.com>
2023-07-12 12:14:14 +0530

    second time

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
   const [time, setTime] = useState(0);
 
   return (
     <div>
+      {/* <p>Hello world!</p> */}
       <p>Button clicked {time} times</p>
       <button onClick={() => {setTime(time+1)}}>Click me</button>
         {/* Do not remove the main div */}
